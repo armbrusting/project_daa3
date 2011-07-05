ce26d1fb4ff772db114908cc23297ce114c77000
ryan armbrust <ryan.armbrust@gmail.com>
2011-07-05 16:55:16 -0400

    User model with passwords, and user show page, including Paperclip

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,17 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    @title = @user.name
   end
   
   def new
     @title = "Sign up"
   end
 
+  #Paperclip change
+  def create
+    @user = User.create( params[:user] )
+  end
+  #end Paperclip change
+  
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,5 +22,8 @@ ProjectDaa3::Application.configure do
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
+  
+  #Paperclip- Ryan Added
+  Paperclip.options[:command_path] = "/opt/local/bin/"
 end
 
