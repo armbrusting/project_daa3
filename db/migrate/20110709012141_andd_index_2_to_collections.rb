1584d6cace3f636ab3cda14d69c7dff9625b31e3
ryan armbrust <ryan.armbrust@gmail.com>
2011-07-28 20:27:39 -0400

    db edits2

--- db/migrate/20110709012141_andd_index_2_to_collections.rb
+++ db/migrate/20110709012141_andd_index_2_to_collections.rb
@@ -1,5 +1,6 @@
 class AnddIndex2ToCollections < ActiveRecord::Migration
   def self.up
+    remove_index :collections, [:collector_id, :collected_id]
     add_index :collections, [:collector_id, :collected_id], :unique => false
   end
 end
