af13480e08c60f2aee88e383a672a906a66da60d
ryan armbrust <ryan.armbrust@gmail.com>
2011-07-28 20:32:28 -0400

    db edits4

--- db/migrate/20110719134737_add_and_remove_things_to_collections_and_folders.rb
+++ db/migrate/20110719134737_add_and_remove_things_to_collections_and_folders.rb
@@ -3,8 +3,8 @@ class AddAndRemoveThingsToCollectionsAndFolders < ActiveRecord::Migration
     remove_column :collections, :attention
     drop_table :notices
     remove_column :folders, :attention
-    remove_column :folders, :attn_monopol,
-    remove_column :folders, :attn_daa,
+    remove_column :folders, :attn_monopol
+    remove_column :folders, :attn_daa
     change_column :collections, :pricing, :decimal, :precision => 8, :scale => 2
     change_column :collections,  :ship_cost, :decimal, :precision => 8, :scale => 2
     add_column :styles, :fabric, :string 
